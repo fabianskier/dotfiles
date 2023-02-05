c54c36e0f205688994c5568259c1ad91cf4d2b90
fabianskier <fabianskier@icloud.com>
2023-02-05 18:59:19 +0100

    CO-0016: Add `alpha` configuration

--- nvim/.config/nvim/lua/plugins/alpha-nvim.lua
+++ nvim/.config/nvim/lua/plugins/alpha-nvim.lua
@@ -1,7 +1,32 @@
+
 return {
   'goolord/alpha-nvim',
     requires = { 'nvim-tree/nvim-web-devicons' },
     config = function ()
-        require'alpha'.setup(require'alpha.themes.startify'.config)
+      local alpha = require("alpha")
+      local dashboard = require("alpha.themes.dashboard")
+
+      -- Set header
+      dashboard.section.header.val = {
+          "                                                     ",
+          "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+          "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+          "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+          "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+          "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+          "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+          "                                                     ",
+      }
+
+      -- Set menu
+      dashboard.section.buttons.val = {
+        dashboard.button("r", "  Recents", ":Telescope oldfiles<CR>"),
+        dashboard.button("f", "  Find File", ":Telescope find_files<CR>"),
+        dashboard.button("g", "  Find Word", ":Telescope live_grep<CR>"),
+        dashboard.button("c", "  Find Command", ":Telescope commands<CR>"),
+        dashboard.button("q", "  Quit", ":qa<CR>"),
+      }
+
+      alpha.setup(dashboard.config)
     end
 }
