1f08eb0750867b6c71266507e58de86b97114b25
fabianskier <fabianskier@icloud.com>
2024-05-08 12:47:23 -0400

    Organize `zsh` folder to use with `stow`
    Signed-off-by: fabianskier <fabianskier@icloud.com>

--- zsh/.oh-my-zsh/custom/aliases.zsh
+++ zsh/.oh-my-zsh/custom/aliases.zsh
@@ -1,7 +1,6 @@
 # docker
 alias up='docker-compose up -d'
 alias down='docker-compose down -v'
-alias reset='docker-compose down -v && docker rmi $(docker images -q) --force && docker-compose up -d'
 
 # zsh
 alias so='source ~/.zshrc'
