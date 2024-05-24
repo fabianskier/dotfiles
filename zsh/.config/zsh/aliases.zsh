f28dcb34fd051574bc3d5bcb91912c2bb605e2df
fabianskier <fabianskier@icloud.com>
2024-05-24 18:59:26 +0100

    Alias to copy environment variable files
    Signed-off-by: fabianskier <fabianskier@icloud.com>

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -4,3 +4,6 @@ alias down='docker-compose down -v'
 
 # zsh
 alias so='source ~/.zshrc'
+
+# custom
+alias cv='cp ~/shared/hqconversions-v2/backend/.env ./backend/.env && cp ~/shared/hqconversions-v2/frontend/.env ./frontend/.env'
