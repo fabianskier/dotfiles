df6e676939076b2451c4250e5fb774ef5f0af782
fabianskier <fabianskier@icloud.com>
2026-04-09 10:49:10 -0300

    feat(zsh): add useful command aliases
    Zoxide aliases:
    - z: Interactive directory selector
    - zi: Same as z (interactive)
    - zl: List all frecuent directories
    
    Git aliases:
    - ga, gc, gp, gs, gb, gco, gd, gl
    
    Directory navigation:
    - .., ..., ...., ~, dotfiles, work
    
    File operations:
    - ll, la, l, lh (using eza)
    - cat (using bat)
    
    Quick shortcuts:
    - cw (cropwatch), ca (anski), cm (crop-monitoring), dt (dotfiles)
    - reload (exec zsh)
    - update, clean (brew)

---
+++ zsh/.config/zsh/conf.d/aliases.zsh
@@ -0,0 +1,54 @@
+# Useful aliases for development workflow
+
+# Zoxide aliases (smart directory jumping)
+alias z='zoxide query --interactive'    # Interactive directory selector
+alias zi='zoxide query --interactive'   # Same as above
+alias zl='zoxide query -l'             # List all frecuent directories
+
+# Git aliases
+alias ga='git add'
+alias gc='git commit'
+alias gp='git push'
+alias gpl='git pull'
+alias gs='git status'
+alias gb='git branch'
+alias gco='git checkout'
+alias gd='git diff'
+alias gl='git log --oneline'
+
+# Directory navigation
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias ~='cd ~'
+alias dotfiles='cd ~/.dotfiles'
+alias work='cd ~/work'
+
+# Listing
+alias ll='eza -lah'           # Long list with hidden
+alias la='eza -lA'            # All files except . and ..
+alias l='eza -l'              # Long list
+alias lh='eza -lh'            # Long list with human readable size
+
+# Utilities
+alias cat='bat'               # Use bat instead of cat
+alias grep='grep --color=auto'
+alias mkdir='mkdir -p'        # Create parent directories
+alias cp='cp -i'              # Ask before overwrite
+alias mv='mv -i'              # Ask before overwrite
+alias rm='rm -i'              # Ask before delete
+
+# Quick edits
+alias zshconfig='nano ~/.zshrc'
+alias dotconfig='cd ~/.dotfiles && nano README.md'
+
+# Shortcuts for work directories
+alias cw='z cropwatch'         # Jump to cropwatch
+alias ca='z anski'             # Jump to anski
+alias cm='z crop-monitoring'   # Jump to crop-monitoring
+alias dt='z dotfiles'          # Jump to dotfiles
+
+# Common commands
+alias reload='exec zsh'        # Reload shell config
+alias update='brew update && brew upgrade'
+alias clean='brew cleanup'
