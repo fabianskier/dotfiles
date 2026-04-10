8218011c6178ebed223ca910fe6b5a613fd43037
fabianskier <fabianskier@icloud.com>
2026-04-10 16:23:06 -0300

    feat: add Catppuccin theme to neovim
    - Setup lazy.nvim plugin manager
    - Configure Catppuccin Mocha colorscheme

---
+++ neovim/.config/nvim/init.lua
@@ -0,0 +1,2 @@
+-- Neovim Configuration
+require("config.lazy")
---
+++ neovim/.config/nvim/lua/config/lazy.lua
@@ -0,0 +1,34 @@
+-- Lazy.nvim Plugin Manager
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugin specifications
+local plugins = {
+  -- Catppuccin colorscheme
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+        transparent_background = false,
+      })
+      vim.cmd("colorscheme catppuccin")
+    end,
+  },
+}
+
+require("lazy").setup(plugins)
